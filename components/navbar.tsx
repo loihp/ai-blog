4033fd9d0737df5794b565b967a5a0852821d6c1
Loi Huynh <huynhphatloi001@gmail.com>
2023-09-12 00:58:11 +0700

    Fix wrong import

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,17 @@
 import { UserButton } from '@clerk/nextjs'
 import MobileSidebar from "@/components/mobile-sidebar";
-
+import { getApiLimitCount } from "@/lib/api-limit";
+import { checkSubscription } from "@/lib/subscription";
 
 const Navbar = () => {
+  const apiLimitCount = await getApiLimitCount();
+  const isPro = await checkSubscription();
+
   return (
     <div className="flex-items-center p-4">
-      <MobileSidebar />
+      <MobileSidebar isPro={isPro} apiLimitCount={apiLimitCount} />
       <div className="flex w-full justify-end">
-        <UserButton afterSignOutUrl='/'/>
+        <UserButton afterSignOutUrl="/" />
       </div>
     </div>
    );
