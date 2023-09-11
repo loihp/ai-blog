24fb8a12165ebe5511fad34a05a4d096e02b3ce7
Loi Huynh <huynhphatloi001@gmail.com>
2023-09-12 01:00:10 +0700

    Fix wrong import

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,10 @@
 import { UserButton } from '@clerk/nextjs'
 import MobileSidebar from "@/components/mobile-sidebar";
-import { getApiLimitCount } from "@/lib/api-limit";
+import { getApiLimit } from "@/lib/api-limit";
 import { checkSubscription } from "@/lib/subscription";
 
 const Navbar = () => {
-  const apiLimitCount = await getApiLimitCount();
+  const apiLimitCount = await getApiLimit();
   const isPro = await checkSubscription();
 
   return (
